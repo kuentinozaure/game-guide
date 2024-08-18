a9f9d12138fd1ba06c2e8a526a2285f57e0c085d
Quentin AUBRY <29443413+kuentinozaure@users.noreply.github.com>
2024-08-17 23:37:22 -0400

    fix: enable cors for localhost

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS
   await app.listen(3000);
 }
 bootstrap();
