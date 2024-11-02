5d671367c30b037e441405404f439b5f66e766f6
Quentin AUBRY <29443413+kuentinozaure@users.noreply.github.com>
2024-11-02 02:33:15 -0400

    fix: wrong port

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const port = process.env.PORT || 3000;
+console.log(
+  `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
+);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -10,6 +15,6 @@ async function bootstrap() {
     }),
   );
   app.enableCors(); // Enable CORS
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
